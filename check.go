1a02b107eea3280f368d705d0ae73b7c254595c9
Marcus Koh <marcus.koh@zepben.com>
2022-09-23 15:15:21 +1000

    Make code more testable, add Zepben License
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- check.go
+++ check.go
@@ -13,7 +13,6 @@ import (
 )
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
@@ -25,28 +24,45 @@ func check(e error) {
 	}
 }
 
-// Simple program for checking for AGPL licences.
-// Takes a single argument: The path of the file to check
-// Returns 0 on success and -1 if neither the AGPL licence nor header snippet did not meet an 80% match.
-// Should be used on either source files with licence headers or COPYING files.
-func main() {
-	var licences = licensecheck.BuiltinLicenses()
-	var gpls []licensecheck.License
-	filepath := os.Args[1]
+func IsValidLicense(filepath string) (bool, error) {
+	license, err := os.ReadFile(filepath)
+	check(err)
 
-	for _, l := range licences {
+	var acceptedLicences []licensecheck.License
+
+	for _, l := range licensecheck.BuiltinLicenses() {
 		if l.Name == "AGPL-Header" || l.Name == "AGPL-v3.0" || l.Name == "MPL-2.0" || l.Name == "MPL-2.0-Header" || l.Name == "MIT" {
-			gpls = append(gpls, l)
+			acceptedLicences = append(acceptedLicences, l)
 		}
 	}
-	checker := licensecheck.New(gpls)
-	file, err := ioutil.ReadFile(filepath)
+	zepbenLicense, err := os.ReadFile("Zepben.lre")
 	check(err)
-	_, succ := checker.Cover(file, licensecheck.Options{MinLength: 10, Threshold: RequiredMatchPercentage, Slop: 8})
+	acceptedLicences = append(acceptedLicences, licensecheck.License{Name: "Zepben", Text: string(zepbenLicense)})
+	checker := licensecheck.New(acceptedLicences)
+
+	_, succ := checker.Cover(license, licensecheck.Options{MinLength: 10, Threshold: RequiredMatchPercentage, Slop: 8})
 	if succ {
+		return true, nil
+	} else {
+		return false, fmt.Errorf("Licence check failed with a <", RequiredMatchPercentage, "% match for", filepath, "Ensure the AGPL or MPL licence is present and correct in the file.")
+	}
+}
+
+// Simple program for checking for accepted licenses. The accepted licenses are:
+//   - AGPL v3
+//   - MPL v2
+//   - MIT
+//   - Zepben (closed source)
+//
+// Takes a single argument: The path of the file to check
+// Returns 0 on success and -1 if neither the AGPL/Zepben licence nor header snippet did not meet an 80% match.
+// Should be used on either source files with licence headers or COPYING files.
+func main() {
+	valid, err := IsValidLicense(os.Args[1])
+	if valid {
 		os.Exit(0)
 	} else {
-		fmt.Println("Licence check failed with a <", RequiredMatchPercentage, "% match for", filepath, "Ensure the AGPL or MPL licence is present and correct in the file.")
+		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
 }
